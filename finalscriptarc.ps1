77c6c6606b6f099969feb23896952fbae0a9dad2
Technical And Cloud <82224047+technicalandcloud@users.noreply.github.com>
2022-11-07 09:03:47 +0100

    Add files via upload

---
+++ finalscriptarc.ps1
@@ -0,0 +1,28 @@
+﻿$CSV = Import-CSV -Path "C:\Users\Administrateur\Desktop\script.csv" -Delimiter ","
+connect-azaccount
+
+
+
+foreach($Ligne in $CSV)
+
+
+
+{
+
+
+
+$Computername=$Ligne.ComputerName
+$resourcegroupe=$ligne.ResourceGroup
+$Region=$ligne.location
+
+
+
+$sessions = New-PSSession -ComputerName $Computername
+
+
+
+Connect-AzConnectedMachine -ResourceGroupName $resourcegroupe -Location $Region -PSSession $sessions
+
+
+
+}
