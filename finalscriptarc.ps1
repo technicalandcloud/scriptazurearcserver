e979c21fd31b673a0838bab85058a9a3f04fbf4e
Technical And Cloud <82224047+technicalandcloud@users.noreply.github.com>
2022-11-07 09:44:29 +0100

    Update finalscriptarc.ps1

--- finalscriptarc.ps1
+++ finalscriptarc.ps1
@@ -1,28 +1,24 @@
-﻿$CSV = Import-CSV -Path "C:\Users\Administrateur\Desktop\script.csv" -Delimiter ","
-connect-azaccount
-
+#import CSV Server
+$CSV = Import-CSV -Path "XXXXXXX\script.csv" -Delimiter ","
+#Connect on Azure 
+connect-azaccount -Subcription XXXXXXXXXX
 
 
 foreach($Ligne in $CSV)
-
-
-
 {
-
-
-
+#select on the columm ComputerName the first Line on the CSV
 $Computername=$Ligne.ComputerName
+#select on the columm Resource Group the first Line on the CSV
 $resourcegroupe=$ligne.ResourceGroup
+#select on the columm Location the first Line on the CSV
 $Region=$ligne.location
 
-
+#Connexion the VM present in ComputerName
 
 $sessions = New-PSSession -ComputerName $Computername
 
-
+#Enable in Azure
 
 Connect-AzConnectedMachine -ResourceGroupName $resourcegroupe -Location $Region -PSSession $sessions
-
-
 
 }
